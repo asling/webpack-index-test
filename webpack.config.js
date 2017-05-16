49bb8acac976a8ddcedbb0fdf31d1b21bd5f1ab3
andy <andy@molimama.cn>
2017-05-16 09:28:34 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: './app.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[hash].bundle.js',
+	},
+	module:{
+		rules:[
+			{
+				test: /\.js$/,
+			    loader: 'babel-loader',
+			    exclude: /node_modules/
+			}
+		]
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+	        template: './index.html',
+	        inject: 'body'
+	      }),
+		new CopyWebpackPlugin([{
+	        from: './index.html'
+	    }])
+
+	]
+}
